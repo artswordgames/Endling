d8ec57bbcd7a76a0dea09686afc42b6970f5ae2d
Andrew Straight <devArtsw0rd7@protonmail.com>
2025-10-31 11:00:11 -0500

    Initial Commit

---
+++ Endling/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Endling
+//
+//  Created by artsw0rd on 10/31/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Endling/EndlingApp.swift
@@ -0,0 +1,17 @@
+//
+//  EndlingApp.swift
+//  Endling
+//
+//  Created by artsw0rd on 10/31/25.
+//
+
+import SwiftUI
+
+@main
+struct EndlingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
